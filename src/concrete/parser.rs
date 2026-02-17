32f188d72ffbae16cd9388c5af06478550534d42
ka <kaku.h9@gmail.com>
2026-02-17 22:20:21 +0000

    concrete: added backtracking to the parser for literal expressions

--- src/concrete/parser.rs
+++ src/concrete/parser.rs
@@ -154,25 +154,25 @@ impl Parser{
 
     let char_literal = self.try_parse_char_literal();
     match char_literal{
-      None => {}
+      None => {self.position = saved_pos;}
       Some(literal_exp) => {return Some(literal_exp);}
     };
     
     let string_literal = self.try_parse_string_literal();
     match string_literal{
-      None => {}
+      None => {self.position = saved_pos;}
       Some(literal_exp) => {return Some(literal_exp);}
     };
 
     let int_literal = self.try_parse_int_literal();
     match char_literal{
-      None => {}
+      None => {self.position = saved_pos;}
       Some(literal_exp) => {return Some(literal_exp);}
     };
 
     let float_literal = self.try_parse_float_literal();
     match char_literal{
-      None => {}
+      None => {self.position = saved_pos;}
       Some(literal_exp) => {return Some(literal_exp);}
     };
     
