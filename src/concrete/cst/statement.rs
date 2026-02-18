b594eea59116be5bbff6e225b3a9b74f541335be
ka <kaku.h9@gmail.com>
2026-02-18 15:55:05 +0000

    concrete: added Print method to CstLetStatement

--- src/concrete/cst/statement.rs
+++ src/concrete/cst/statement.rs
@@ -6,3 +6,20 @@ pub struct CstLetStatement{
   pub let_type: Option<String>,                // type is a string, semantic analsyer phase can handle validity of types
   pub expression: Option<expressions::CstExpression> 
 }
+
+impl CstLetStatement{
+  pub fn Print(&self){
+    println!("Let Statement");
+
+    print!("let Type: ");
+    match &self.let_type{
+      None => println!("no type annotation provided"),
+      Some(let_type) => println!("type: {}", &let_type)
+    }
+
+    match &self.expression{
+      Some(expression) => expression.Print()
+      None => println!("no expression provided"),
+    }
+  }
+}
